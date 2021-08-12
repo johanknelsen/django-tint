432f4cb3b08790de88bda5f461c3d21f7fee85bd
johanknelsen <46053897+johanknelsen@users.noreply.github.com>
2021-08-12 18:19:46 -0400

    Add files via upload

--- tintstore/tint/admin.py
+++ tintstore/tint/admin.py
@@ -3,19 +3,22 @@ from django.contrib import admin
 # Register your models here.
 from .models import Car, Tint, TintTask
 
-class CarInline(admin.TabularInline):
-    model = Car
 
-class TintTaskAdmin(admin.ModelAdmin):
-    # inlines = [
-    #     CarInline,
-    # ]
+
+class TintTaskAdmin(admin.ModelAdmin):    
 
     def get_changeform_initial_data(self, request):
         get_data = super(TintTaskAdmin, self).get_changeform_initial_data(request)
         get_data['tinter'] = request.user.pk
         return get_data
 
-admin.site.register(Car)
+class TaskInline(admin.TabularInline):    
+    model = TintTask
+
+class CarAdmin(admin.ModelAdmin):
+    inlines = [TaskInline]
+
+
+admin.site.register(Car, CarAdmin)
 admin.site.register(Tint)
 admin.site.register(TintTask, TintTaskAdmin)
